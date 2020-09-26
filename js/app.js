5ce46aa5565bf2813fa737963e9b7a6302b77a37
Gor <g.var.design@gmail.com>
2020-09-26 15:05:10 +0200

    created page 3 interface and player choosed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-                     // seltor de personajes
+                 //fix players
 
 const selector = (num) =>{
     let first_selector = document.getElementById("first_selector");
@@ -10,6 +10,7 @@ const selector = (num) =>{
         first_selector.style.display="none";
         second_selector.style.display="flex";
 
+        document.getElementById('user1').innerHTML = player_1.renderPlayer();
         //contenido first selector actua al click
         document.getElementById('player_picked').style.backgroundColor='gray';
         document.getElementById('fijar_player').style.backgroundColor='gray';
@@ -21,7 +22,8 @@ const selector = (num) =>{
         //contenido second selector actua al click
         document.getElementById('cpu_picked').style.backgroundColor='gray';
         document.getElementById('fijar_cpu').style.backgroundColor='gray';
-       
+        document.getElementById('PC').innerHTML = player_2.renderPlayer();
+        
         //si first y second selector están con display none accede
         if(first_selector.style.display === "none" && second_selector.style.display === "none"){
             //aparece el botón para pasar a la fase3
@@ -30,25 +32,33 @@ const selector = (num) =>{
     }
 }
 
-                     // fijación de personaje 
+                //player selector
+let player_1 = {};
+let player_2 = {};
 
-const fix = (num)=>{
+const player_selector = (name,num)=>{
 
     switch(num){
-         case 1:
+        case 1:
+            player_1 = new personajes(name,1);
+            console.log(`${name} player selecionado`);
             document.getElementById('fijar_player').style.display="block";
             document.getElementById('player_picked').style.visibility="visible"; 
-            
-             break;
+            document.getElementById('first_player_stats').innerHTML = player_1.renderStats();
+            break;
 
-         case 2: 
-             document.getElementById('fijar_cpu').style.display="block";
-             document.getElementById('cpu_picked').style.visibility="visible"; 
-             break;
-     }
+        case 2: 
+            player_2 = new personajes(name,2);
+            console.log(`${name} cpu selecionado`);
+            document.getElementById('fijar_cpu').style.display="block";
+            document.getElementById('cpu_picked').style.visibility="visible"; 
+            document.getElementById('second_player_stats').innerHTML = player_2.renderStats();
+            break;   
+    }
 }
 
-                     // cambiar de pantallas
+
+             //cambiar de pantallas
 
 const cambia_pantalla = (valor) =>{
   
@@ -66,16 +76,16 @@ const cambia_pantalla = (valor) =>{
     }
 }
  
-                         // class de personajes
+                //class de personajes
 
 class personajes {
-    constructor (nombre){
-        this.img_directory = "img/" + nombre + "posicion_base.gif";
+    constructor (nombre,rotate){
+        this.name = nombre;
+        this.img_directory = "img/" + nombre + "/posicion_base"+ rotate +".gif";
+        this.img_directoryRotate = "img/" + nombre + "/posicion_base2.gif";
         this.max_health = 100;
         this.health = 100;
-        this.mana = 50;
-        this.power = 50;
-        this.magic = 50;
+        this.power = 60;
     }
 
     addPower(number) {
@@ -90,4 +100,14 @@ class personajes {
         return (this.health*this.max_health)/100;
     }
     
+    renderStats(){
+        return `<h2>${this.name.toUpperCase()}</h2>
+                <p>Health: ${this.health}</p>
+                <p>Power: ${this.power}</p>`;
+    }
+    renderPlayer(){
+        return `<img src="${this.img_directory}">
+                <p>Name: ${this.name}</p>
+                <p>Health: ${this.health}</p>`;
+    }
 }
